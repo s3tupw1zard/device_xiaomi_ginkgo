76decff70efb4d5679f4efc758c6b3f42e455c35
NitroFuN <theedge5670@gmail.com>
2020-08-01 19:53:20 +0200

    ginkgo: Go official!
    * And also enable support for face unlock

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -18,6 +18,8 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_ginkgo
