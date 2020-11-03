656b4dd45201a3703d4dc0393bcd7f688002beb1
NitroFuN <theedge5670@gmail.com>
2020-11-03 12:56:20 +0100

    ginkgo: init: Override rom device prop if device is willow
    * Just a cosmetic thing

--- init/init_ginkgo.cpp
+++ init/init_ginkgo.cpp
@@ -46,6 +46,7 @@ void vendor_load_properties()
     if (region == "Global_B" && (hwversion == "18.39.0" || hwversion == "19.39.0")) {
         device = "willow";
         model = "Redmi Note 8T";
+        property_override("ro.ssos.device", device);
     } else {
         device = "ginkgo";
         model = "Redmi Note 8";
